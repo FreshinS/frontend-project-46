dd3cfe94a9fe8271a8738fe09b01ac8a7c210234
FreshinS <freshinsboost@gmail.com>
2024-05-14 11:48:43 +0300

    step 1 fixes

--- gendiff.js
+++ gendiff.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
-
 import { Command } from 'commander';
+
 const program = new Command();
 
 program
-    .description('Compares two configuration files and shows a difference.')
-    .version('0.0.1', '-v, --version', 'output the version number');
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1', '-v, --version', 'output the version number')
+  .option('f, --format [type]', 'output format')
+  .argument('[type]');
 
 program.parse();
