e0918854603297d9ebef0a4b4f06eba3fd458d64
FreshinS <freshinsboost@gmail.com>
2024-10-04 14:02:08 +0300

    test for step7

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,24 +3,24 @@
 import * as fs from 'node:fs';
 import * as path from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { mergeKeys, indent, printDiff, genDiff } from '../src/index.js';
 import { jest } from '@jest/globals';
-import { mergeKeys, indent, genDiff } from '../src/index.js';
 
 describe('mergeKeys', () => {
-  it('should merge two arrays of keys and return sorted unique values', () => {
+  it('совмещает два массива и сортирует их', () => {
     const keys1 = ['a', 'b', 'c'];
     const keys2 = ['b', 'c', 'd', 'e'];
     const result = mergeKeys(keys1, keys2);
     expect(result).toEqual(['a', 'b', 'c', 'd', 'e']);
   });
 
-  it('should handle empty arrays', () => {
+  it('работает с пустыми массивами', () => {
     expect(mergeKeys([], ['a', 'b'])).toEqual(['a', 'b']);
     expect(mergeKeys(['a', 'b'], [])).toEqual(['a', 'b']);
     expect(mergeKeys([], [])).toEqual([]);
   });
 
-  it('should return sorted unique values when arrays have common elements', () => {
+  it('не содержит одинаковых элементов', () => {
     const keys1 = ['a', 'b'];
     const keys2 = ['b', 'c', 'd'];
     const result = mergeKeys(keys1, keys2);
@@ -29,27 +29,117 @@ describe('mergeKeys', () => {
 });
 
 describe('indent', () => {
-  it('should return the correct indentation string', () => {
+  it('возвращает правильное количество отступов', () => {
     expect(indent(1)).toBe('    ');
     expect(indent(2)).toBe('        ');
     expect(indent(1, 2)).toBe('  ');
-    expect(indent(1, 10)).toBe('');
+    expect(indent(1, 0, 2)).toBe('  ');
+    expect(indent(1, 1, 2)).toBe(' ');
   });
 
-  it('should return an empty string when repeats is negative', () => {
+  it('выводит пустую строку при значении сдвига влево больше чем количество оступов', () => {
     expect(indent(1, 10)).toBe('');
   });
 });
 
 describe('genDiff', () => {
-  const consoleSpy = jest.spyOn(console, 'log');
+  it('работает с пустыми файлами', () => {
+    const logSpy = jest.spyOn(process.stdout, 'write').mockImplementation();
 
-  test('тестирование вывода в консоль', () => {
-    console.log('Hello, World!');
-    expect(consoleSpy).toHaveBeenCalledWith('Hello, World!');
+    const obj1 = {};
+    const obj2 = {};
+
+    genDiff(obj1, obj2);
+
+    expect(logSpy.mock.calls).toEqual([
+      ['{\n'],
+      ['}\n']
+    ]);
+
+    logSpy.mockRestore();
   });
+  it('работает с простыми объектами', () => {
+    const logSpy = jest.spyOn(process.stdout, 'write').mockImplementation();
 
-  afterAll(() => {
-    consoleSpy.mockRestore();
+    const obj1 = {
+      a: 1,
+      b: 2,
+      c: 3
+    };
+    const obj2 = {
+      a: 1,
+      b: 3,
+      d: 5
+    };
+
+    genDiff(obj1, obj2);
+
+    expect(logSpy.mock.calls).toEqual([
+      ['{\n'],
+      ['    a: 1\n'],
+      ['  - b: 2\n'],
+      ['  + b: 3\n'],
+      ['  - c: 3\n'],
+      ['  + d: 5\n'],
+      ['}\n']
+    ]);
+
+    logSpy.mockRestore();
+  });
+  it('работает с вложенными объектами', () => {
+    const logSpy = jest.spyOn(process.stdout, 'write').mockImplementation();
+
+    const obj1 = {
+      a: 1,
+      b: {
+        b1: 2,
+        b2: {
+          b21: {
+            b212: 5
+          },
+          b23: 5
+        }
+      },
+      c: 3
+    };
+    const obj2 = {
+      a: 1,
+      b: {
+        b1: 1,
+        b2: {
+          b21: 3,
+          b23: 4
+        }
+      },
+      d: {
+        d1: 1
+      }
+    };
+
+    genDiff(obj1, obj2);
+
+    expect(logSpy.mock.calls).toEqual([
+      ['{\n'],
+      ['    a: 1\n'],
+      ['    b: {\n'],
+      ['      - b1: 2\n'],
+      ['      + b1: 1\n'],
+      ['        b2: {\n'],
+      ['          - b21: {\n'],
+      ['                b212: 5\n'],
+      ['            }\n'],
+      ['          + b21: 3\n'],
+      ['          - b23: 5\n'],
+      ['          + b23: 4\n'],
+      ['        }\n'],
+      ['    }\n'],
+      ['  - c: 3\n'],
+      ['  + d: {\n'],
+      ['      d1: 1\n'],
+      ['    }\n'],
+      ['}\n']
+    ]);
+
+    logSpy.mockRestore();
   });
 });
--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ export const indent = (it, left = 0, i = 4) => {
   return ' '.repeat(repeats);
 };
 
-const logDiff = (key, data, it, s = ' ') => {
+export const printDiff = (key, data, it, s = ' ') => {
   if (_.isObject(data[key])) {
     process.stdout.write(`${indent(it, 2)}${s} ${key}: `);
     genDiff(data[key], data[key], it + 1);
@@ -35,14 +35,44 @@ export const genDiff = (data1, data2, it = 1) => {
       } else if (data1[key] === data2[key]) {
         process.stdout.write(`${indent(it, 2)}  ${key}: ${data1[key]}\n`);
       } else {
-        logDiff(key, data1, it, '-');
-        logDiff(key, data2, it, '+');
+        printDiff(key, data1, it, '-');
+        printDiff(key, data2, it, '+');
       }
     } else if (keys1.includes(key)) {
-      logDiff(key, data1, it, '-');
+      printDiff(key, data1, it, '-');
     } else if (keys2.includes(key)) {
-      logDiff(key, data2, it, '+');
+      printDiff(key, data2, it, '+');
     }
   });
   process.stdout.write(`${indent(it - 1)}}\n`);
 };
+
+const obj1 = {
+  a: 1,
+  b: {
+    b1: 2,
+    b2: {
+      b21: {
+        b212: 5
+      },
+      b23: 5
+    }
+  },
+  c: 3
+};
+const obj2 = {
+  a: 1,
+  b: {
+    b1: 1,
+    b2: {
+      b21: 3,
+      b23: 4
+    }
+  },
+  d: {
+    d1: 1
+  }
+};
+
+
+genDiff(obj1, obj2);
